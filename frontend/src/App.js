8c05845e33baceab807b221d96c49278da9c628b
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-03-03 08:33:08 +0530

    Update map location and marker style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,22 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
+  const temp = 10;
   return (
     <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      initialViewState={{
-        longitude: 48.858093,
-        latitude: 2.294694,
-        zoom: 8,
-      }}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    >
-      <Marker longitude={16.93} latitude={82.22} anchor="bottom">
-        <LocationOnIcon />
-      </Marker>
-    </Map>
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    initialViewState={{
+      latitude: 19.0760,
+      longitude: 72.8777,
+      zoom: temp,
+    }}
+    style={{ width: "100vw", height: "100vh" }}
+    mapStyle="mapbox://styles/varun2222/cltax7ug500gd01qn0x60alup"
+  >
+    <Marker latitude={48.8584} longitude={2.2945} anchor="bottom">
+      <LocationOnIcon style={{fontSize: temp * 4, color:"darkgrey"}} />
+    </Marker>
+  </Map>
   );
 }
 
