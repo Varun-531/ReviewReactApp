ae7096179ae1abe58f7df070ca8078eacfac0e1c
Varun-531 <21pa1a0531@vishnu.edu.in>
2024-03-03 09:08:56 +0530

    Update Map Component in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
 import * as React from "react";
-import Map, { Marker } from "react-map-gl";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { useState } from "react";
+import Map, { Marker, Popup } from "react-map-gl";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import StarIcon from '@mui/icons-material/Star';
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   const temp = 10;
+  const [showPopup, setShowPopup] = useState(true);
   return (
     <Map
-    mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-    initialViewState={{
-      latitude: 19.0760,
-      longitude: 72.8777,
-      zoom: temp,
-    }}
-    style={{ width: "100vw", height: "100vh" }}
-    mapStyle="mapbox://styles/varun2222/cltax7ug500gd01qn0x60alup"
-  >
-    <Marker latitude={48.8584} longitude={2.2945} anchor="bottom">
-      <LocationOnIcon style={{fontSize: temp * 4, color:"darkgrey"}} />
-    </Marker>
-  </Map>
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      initialViewState={{
+        latitude: 19.076,
+        longitude: 72.8777,
+        zoom: temp,
+      }}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/varun2222/cltax7ug500gd01qn0x60alup"
+    >
+      <Marker latitude={48.8584} longitude={2.2945} anchor="bottom">
+        <LocationOnIcon style={{ fontSize: temp * 4, color: "darkgrey" }} />
+      </Marker>
+      {showPopup && (
+        <Popup
+          latitude={48.8584}
+          longitude={2.2945}
+          anchor="left"
+          onClose={() => setShowPopup(false)}>
+          <div className="card">
+          <label>Place</label>
+          <h4 className="place">Effiel Tower</h4>
+          <label>Review</label>
+          <p>It was beautiful</p>
+          <label>Rating</label>
+          <div>
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          <StarIcon />
+          </div>
+          <label>Information</label>
+          </div>
+        </Popup>
+      )}
+    </Map>
   );
 }
 
